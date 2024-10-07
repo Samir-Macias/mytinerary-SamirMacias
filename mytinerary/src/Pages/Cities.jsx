930e62da8d755a75a6f4f69af983d498b977ac9f
Samir <samirmh15@gmail.com>
2024-10-07 19:11:49 +0200

    s

--- mytinerary/src/Pages/Cities.jsx
+++ mytinerary/src/Pages/Cities.jsx
@@ -4,7 +4,9 @@ export default function Cities () {
     return (
         <>
         <div className="bg-slate-500 h-80">
-        
+        <button>
+            
+        </button>
         </div>
         </>
     )
